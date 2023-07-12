8dfe04ffafa3251ff38356ef72b547aa500138d6
Fehmi Tahsin Demirkan <94235535+Fehmi2003@users.noreply.github.com>
2023-07-12 10:46:23 +0300

    Delete javaExample/app/src/main/java/com/fehmitd/javaexample directory

--- javaExample/app/src/main/java/com/fehmitd/javaexample/Main.java
+++
@@ -1,9 +0,0 @@
-package com.fehmitd.javaexample;
-
-public class Main {
-    public static  void  main(String[]args){
-        System.out.println("Hello Gedik");
-        System.out.println(5*2);
-        System.out.println(5*2);
-    }
-}
