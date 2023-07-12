3c30e405a8e0d74f827315627d013b54b4770feb
Fehmi Tahsin Demirkan <fehmitahsindemirkan@gmail.com>
2023-07-12 10:41:33 +0300

    arraylist

---
+++ javaExample/app/src/main/java/com/fehmitd/javaexample/Main.java
@@ -0,0 +1,9 @@
+package com.fehmitd.javaexample;
+
+public class Main {
+    public static  void  main(String[]args){
+        System.out.println("Hello Gedik");
+        System.out.println(5*2);
+        System.out.println(5*2);
+    }
+}
