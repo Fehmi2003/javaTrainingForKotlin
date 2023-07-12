4432b3ec05a7626c4887313de97f08f9ff89c62b
Fehmi Tahsin Demirkan <fehmitahsindemirkan@gmail.com>
2023-07-12 11:46:28 +0300

    arraylist

---
+++ javaTraining/app/src/main/java/com/fehmitd/javatraining/Main.java
@@ -0,0 +1,8 @@
+package com.fehmitd.javatraining;
+
+public class Main {
+    public static void main(String[]args){
+        System.out.println("Hello Gedik");
+        System.out.println(8*2);
+    }
+}
