f1c227aa514c69370c8a1b0a58f778971d661be8
Fehmi Tahsin Demirkan <fehmitahsindemirkan@gmail.com>
2023-07-12 15:31:21 +0300

    javacodes

--- javaTraining/app/src/main/java/com/fehmitd/javatraining/Main.java
+++ javaTraining/app/src/main/java/com/fehmitd/javatraining/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[]args){
         System.out.println("Hello Gedik");
         System.out.println(8*2);
+        System.out.println("sa");
     }
 }
